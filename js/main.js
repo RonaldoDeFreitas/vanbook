8a7e76a784a5013a53bbb38964c8723565bcf0ad
Ronaldo de Freitas <freitasderonaldo@gmail.com>
2017-11-04 12:16:06 -0200

    adding the files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('a.btn').on('click', function(e){
+	e.preventDefault();
+	e.preventDefault();
+	$('.modal').addClass('modal-ativo')
+})
+
+$('.modal').on('click', function(modal){
+	if(modal.target == this) {
+		$(this).removeClass('modal-ativo');
+	}
+});
